4852894d88d8dab5161054f8ed75ced329c10be2
Dada <63845798+andreadada@users.noreply.github.com>
2026-08-12 13:13:35 +0200

    feat: classify MCA vampire lifecycle transitions

---
+++ src/main/java/com/darkfolklore/core/lifecycle/McaVampireLifecycleTransition.java
@@ -0,0 +1,15 @@
+package com.darkfolklore.core.lifecycle;
+
+public enum McaVampireLifecycleTransition {
+    NONE,
+    INFECTION_STARTED,
+    NATIVE_BITE_CONVERTED,
+    CONVERTED,
+    INHERITED_VAMPIRE,
+    CURE_STARTED,
+    CURE_CANCELLED,
+    CURED,
+    INFECTION_CLEARED,
+    VAMPIRISM_CLEARED,
+    UNCLASSIFIED
+}
