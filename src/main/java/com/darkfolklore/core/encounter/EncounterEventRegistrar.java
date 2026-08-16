534da636b50b0a768506bec05a9e03f2cdd3bbf1
Dada <63845798+andreadada@users.noreply.github.com>
2026-08-16 14:52:02 +0200

    Remove pre-commit encounter mutations from entity join

--- src/main/java/com/darkfolklore/core/encounter/EncounterEventRegistrar.java
+++ src/main/java/com/darkfolklore/core/encounter/EncounterEventRegistrar.java
@@ -3,7 +3,6 @@ package com.darkfolklore.core.encounter;
 import com.darkfolklore.core.DarkFolkloreCore;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
 
 @EventBusSubscriber(modid = DarkFolkloreCore.MOD_ID)
@@ -13,10 +12,5 @@ public final class EncounterEventRegistrar {
     @SubscribeEvent
     public static void onPositionCheck(MobSpawnEvent.PositionCheck event) {
         ThreatPolicyRuntime.INSTANCE.onPositionCheck(event);
-    }
-
-    @SubscribeEvent
-    public static void onEntityJoin(EntityJoinLevelEvent event) {
-        ThreatPolicyRuntime.INSTANCE.onEntityJoin(event);
     }
 }
